f07780f5a3ebf6f180fca5f100cd30015a56694d
Adetola Adeya <detolaadeya@gmail.com>
2023-03-20 19:55:53 +0100

    next commit

--- deployment-for-both-apps/provider.tf
+++ deployment-for-both-apps/provider.tf
@@ -21,15 +21,6 @@ data "aws_eks_cluster_auth" "oneapp" {
   name = "oneapp"
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.oneapp.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.oneapp.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.oneapp]
-    command     = "aws"
-  }
-}
 
 resource "kubernetes_namespace" "kube-namespace-socks" {
   metadata {
