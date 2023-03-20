5a933d7c0f8f122999619ec790372eb64f6f3397
Adetola Adeya <detolaadeya@gmail.com>
2023-03-20 20:01:25 +0100

    next commit

--- deployment-for-both-apps/provider.tf
+++ deployment-for-both-apps/provider.tf
@@ -27,3 +27,14 @@ resource "kubernetes_namespace" "kube-namespace-socks" {
     name = "sock-shop"
   }
 }
+
+provider "kubectl" {
+  load_config_file  = false
+  host                   = data.aws_eks_cluster.eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks-cluster.name]
+    command     = "aws"
+  }
+}
