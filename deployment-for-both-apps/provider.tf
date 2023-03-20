427c660960bef7c9f819b40f39381a4e740de0ab
Adetola Adeya <detolaadeya@gmail.com>
2023-03-20 21:14:42 +0100

    forward

--- deployment-for-both-apps/provider.tf
+++ deployment-for-both-apps/provider.tf
@@ -24,7 +24,6 @@ terraform {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.oneapp.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.oneapp.certificate_authority[0].data)
-  version          = "2.16.1"
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
@@ -36,7 +35,9 @@ provider "kubernetes" {
 # Kubectl provider configuration
 
 
-
+data "aws_eks_cluster_auth" "oneapp" {
+  name = "oneapp"
+}
 
 provider "kubectl" {
   load_config_file  = false
