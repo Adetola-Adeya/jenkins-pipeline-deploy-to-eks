9f163f3d816ceb73ee20a50610152cb2fd6e4752
Adetola Adeya <detolaadeya@gmail.com>
2023-03-20 12:26:38 +0100

    youn

--- deployment-for-both-apps/provider.tf
+++ deployment-for-both-apps/provider.tf
@@ -24,17 +24,6 @@ data "aws_eks_cluster_auth" "oneapp" {
   name = "oneapp"
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.oneapp.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.oneapp.certificate_authority[0].data)
-  # version          = "2.16.1"
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.oneapp.name]
-    command     = "aws"
-  }
-}
 
 
 resource "kubernetes_namespace" "kube-namespace-socks" {
