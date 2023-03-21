805ee9239008fddb60238880bc4879131c56ce66
Adetola Adeya <detolaadeya@gmail.com>
2023-03-21 09:26:38 +0100

    Add files via upload

---
+++ infrastructure/eks-cluster.tf
@@ -0,0 +1,42 @@
+module "eks" {
+    source  = "terraform-aws-modules/eks/aws"
+    version = "~> 19.0"
+    cluster_name = "socks-web-shop"
+    cluster_version = "1.24"
+
+    cluster_endpoint_public_access  = true
+
+    vpc_id = module.myapp-vpc.vpc_id
+    subnet_ids = module.myapp-vpc.public_subnets
+
+    tags = {
+        environment = "development"
+        application = "myapp"
+    }
+
+    eks_managed_node_groups = {
+        dev = {
+            min_size = 1
+            max_size = 3
+            desired_size = 3
+
+            instance_types = ["t3.medium"]
+        }
+    }
+
+   
+}
+
+# resource "kubernetes_namespace" "sock-shop" {
+#   metadata {
+#     name = "sock-shop"
+#   }
+
+#   lifecycle {
+#     ignore_changes = [metadata]
+#   }
+# }
+
+ output "eks-cluster" {
+        value = module.eks.cluster_name
+}
